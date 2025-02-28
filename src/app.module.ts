1fe6103f62461f3c38dbb9e2f3bf057f40e7b613
react-native <react-native@react-natives-Mac-mini.local>
2025-02-28 15:55:53 +0530

    changed user module location

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './modules/user/user.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
