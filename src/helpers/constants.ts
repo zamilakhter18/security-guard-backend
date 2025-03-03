ab377a324a8081bf5aecbaad29deef7d4752782e
react-native <react-native@react-natives-Mac-mini.local>
2025-03-03 16:29:51 +0530

    Updated constants.ts with latest enums

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -6,6 +6,52 @@ export enum userTypeEnum {
 }
 
 export enum serviceLookingForEnum {
- 
+  ARMED_SECURITY  = 'armedSecurity',
+  UNARMED_SECURITY = 'unarmedSecurity',
+  BUSINESS_OR_HOME_SAFETY_CHECKS = "businessOrHomeSafetyChecks ",
+  ESCORT_SERVICE = 'escortServices',
+  ALARM_RESPONSE = 'alarmResponse',
+  NEIGHBORHOOD_WATCH_AREA_PATROL = 'neighborhoodWatchAreaPatrol'
+}
+
+export enum socialLoinTypeEnum {
+  GOOGLE = 'google',
+  APPLE = 'apple',
+  FACEBOOK = 'facebook',
+}
+
+export enum servicesLookingForEnum {
+  SECURITY_GUARD = 'securityGuard',
+  PRIVATE_INVESTIGATOR = 'privateInvestigator',
+  SECURITY_TRANSTAPORTATION = 'securityTransportation',
+  RAPID_RESPONSE_REQUEST = 'rapidResponseRequest',
+}
+
+export enum deviceTypeEnum {
+  IOS = 'ios',
+  ANDROID = 'android',
+}
+
+export enum paymentStatusEnum {
+  PENDING = 'pending',
+  SUCCESS = 'success',
+  FAILED = 'failed',
+}
+
+export enum bookingStatusEnum {
+  PENDING = 'pending',
+  ACCEPTED = 'accepted',
+  REJECTED = 'rejected',
+  CANCELED = 'canceled',
+}
+
+export enum cancelReasonEnum {
+  OTHER = 'other',
+}
+
+export enum refundStatus {
+  PENDING = 'pending',
+  SUCCESS = 'success',
+  FAILED = 'failed',
 }
 
