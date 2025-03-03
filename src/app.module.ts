ae7b4bac9d1c6bed9c223be589dff834e57c1403
react-native <react-native@react-natives-Mac-mini.local>
2025-03-03 17:54:13 +0530

    add mongoose configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory:(configService : ConfigService) =>{
+        const dbUserName = configService.get('DATABASE_USERNAME');
+        const dbPassword = configService.get('DATABASE_PASSWORD');
+        const dbHost = configService.get('DATABASE_HOST');
+        const dbName = configService.get('DATABASE_NAME');
+        const uri = `mongodb+srv://${dbUserName}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
+        return { uri }
+      },
+      inject : [ConfigService]
+
     }),
     UserModule,
   ],
